3610f09dbbc17be514e30b0d44c30fccee15b7b7
mixanBit <65313661+mixanBit@users.noreply.github.com>
2021-11-06 16:32:57 +0300

    Перенос звука плеера в новый файл

---
+++ scripts/range-audio.js
@@ -0,0 +1,19 @@
+// let rangeAudio = document.getElementById('rangeAudio')
+//     audio.volume = 0.2
+
+
+// rangeAudio.onchange = function (e) {
+//   console.log(e.target.value)
+//   audio.volume = e.target.value / 100;
+// }
+
+// jQuery
+// $('#rangeAudio[type=range]').on('input', function(e){
+//   var min = e.target.min,
+//       max = e.target.max,
+//       val = e.target.value;
+
+//   $(e.target).css({
+//     'backgroundSize': (val - min) * 100 / (max - min) + '% 100%'
+//   });
+// })
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,10 @@
 let audio = new Audio("./music/audio1.mp3")
-    // rangeAudio = document.getElementById('rangeAudio')
     play = document.getElementById('playAudio')
 
     prevAudio = document.getElementById('prevAudio')
     nextAudio = document.getElementById('nextAudio')
     playing = true
     idAudio = 0
-    // audio.volume = 0.2
-
-// rangeAudio.onchange = function (e) {
-//   console.log(e.target.value)
-//   audio.volume = e.target.value / 100;
-// }
 
 play.addEventListener('click', () => {
   if (playing) {
@@ -26,7 +19,6 @@ play.addEventListener('click', () => {
   }
   playing = !playing;
 })
-
 
 
 nextAudio.addEventListener('click', () => {
@@ -71,17 +63,3 @@ function prevAudios() {
   audio.play()
   play.style = 'background-image: url(./image/pause.png);'
 }
-
-
-// input range
-// $('#rangeAudio[type=range]').on('input', function(e){
-//   var min = e.target.min,
-//       max = e.target.max,
-//       val = e.target.value;
-
-//   $(e.target).css({
-//     'backgroundSize': (val - min) * 100 / (max - min) + '% 100%'
-//   });
-// })
-
-
